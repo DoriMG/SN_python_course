16d3c1a5774c09bc81aa5cb8d2b8d480d06043a5
Dori Grijseels <DoriekeG@users.noreply.github.com>
2019-08-29 18:25:04 +0100

    Lecture slides and notebooks

--- notebooks/example_code/function_answer.py
+++
@@ -1,8 +0,0 @@
-def sum_numbers(n):
-    if n is None:
-        return None
-    if type(n) is not int:
-        raise TypeError()
-    if n <= 0:
-        return 0
-    return sum(list(range(n+1)))
